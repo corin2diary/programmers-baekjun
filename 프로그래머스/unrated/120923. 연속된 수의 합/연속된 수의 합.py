0e2ef872bb95ef838db39aeed20e02688f1cc18c
corin2diary <153697943+corin2diary@users.noreply.github.com>
2023-12-20 00:36:16 +0900

    Update 연속된 수의 합.py

--- 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.py
@@ -4,3 +4,9 @@ def solution(num, total):
     for i in range(x, x+num):
         result.append(i)
     return result
+
+## 문제 풀이 ##
+# total은 x + x+1 + x+2 + x+3... 이런식으로 이루어짐
+# 위 식을 정리하면, total = num(x) + sum(num-1) 이런식으로 정리가 됨.
+# 여기서 x의 값을 추출하면, x = (total-sum(num-1))n 공식이 성립함.
+# 이를 코드로 환산하면, (total - sum(range(num)/n)이 됨.
